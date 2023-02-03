71910cfab43997c3ba6ad0a5b5bbd858a05461d9
Nikolai Lorentzen <nikolai.lorentzen@gmail.com>
2023-02-03 20:26:29 +0100

    constructor now throws Exception if MaxForce given is negative

--- MyApp/Tugboat.cs
+++ MyApp/Tugboat.cs
@@ -2,16 +2,19 @@ namespace VesselsNS
 {
     // Vessel type Tugboat
     public class Tugboat : Vessel {
-        private int max_force;
+        private int maxForce;
 
-        public Tugboat(string Name, int Year, double MaxSpeed, int Max_force, string Format = "KN") 
+        public Tugboat(string Name, int Year, double MaxSpeed, int MaxForce, string Format = "KN") 
         : base(Name, Year, MaxSpeed, Format = "KN") {
-            max_force = Max_force;
+            maxForce = MaxForce;
+
+            // Check that maxForce is not negative
+            if (maxForce < 0) throw new Exception("Max force cannot be negative");
         }
 
         // return string decribing the vessel
         public string GetVesselInfo() {
-            return $"Type: Tugboat, Name: {GetName()}, Year: {GetYearBuilt()}, Max Force: {max_force}";
+            return $"Type: Tugboat, Name: {GetName()}, Year: {GetYearBuilt()}, Max Force: {maxForce}";
         }
     }
 }
